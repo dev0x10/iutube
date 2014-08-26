3bea551637e3fa0666bdab166ca0c4e4010d614d
Yauri <yauri@live.com>
2014-08-26 23:39:12 +0800

    fixing race condition on loading youtube API

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -28,8 +28,10 @@ app.config(function($httpProvider) {
 });
 
 app.run(function() {
-  var tag = document.createElement('script');
-  tag.src = "https://www.youtube.com/iframe_api";
-  var firstScriptTag = document.getElementsByTagName('script')[0];
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    setTimeout(function(){
+        var tag = document.createElement('script');
+        tag.src = "https://www.youtube.com/iframe_api";
+        var firstScriptTag = document.getElementsByTagName('script')[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    }, 500);
 });
